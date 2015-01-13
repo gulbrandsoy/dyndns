f826207fe6aa23991faa428c5d6de4f3a0081e1a
Rune Gulbrandsøy <rune@Frodo.local>
2015-01-13 11:13:49 +0100

    Removed instructions
    Removed instructions and placed them in readme.md

--- dyndns.sh
+++ dyndns.sh
@@ -2,23 +2,7 @@
 
 # How to use
 
-# All commands are in quotes ("")!
-# Set your API key, which can be found at https://manager.linode.com/profile with  "echo MY_API_KEY > .key"
-# Get your Domain ID and your Resource ID
-# For Domain ID run the command "./dyndns.sh list_domains" 
-# For Resource ID run the command "./dyndns.sh list_resources DOMAIN_ID" - where DOMAIN_ID is the number from the command above!
-# Do this for all the domains/resources you want to update. Tips: write them down as you go along :-)
-# Then you have to create your domains and resource files.
-# Like this : "echo xxx,yyy,zzz > .domains" and "echo aaa,bbb,ccc > .resources" where 'xxx,yyy...' and 'aaa,bbb...' are your domain and resource IDs
-#
-# Make a cron job with the script eg "crontab -e" with the command "0,30       *   *   *   *   /path/to/script/dyndns.sh update"
-# The entry above will run the script once every 30 minutes.
-#
-# To get a list of your active domain and resource IDs and active IP run the command "./dyndns.sh dns_info"
-#
-# Original script by Andrew Childs (https://github.com/andrewchilds/linode-dyn-dns)
-# Modified by Rune Gulbrandsøy (http://ghostblog.be)
-
+# See the readme.md file
 
 ##### NO CONFIGURATION NECESSARY.
 declare -a DOMAINIDS
